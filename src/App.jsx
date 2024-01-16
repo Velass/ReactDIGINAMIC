273354d4e74f7590dc8836e18c04bb757dfa45f6
florian <furnari.florian@hotmail.fr>
2024-01-16 17:49:49 +0100

    mise en route du router si on click sur livres ca fonctionne

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { BooksProvider } from "./utils/context/BooksContext";
 import BooksPage from "./components/books/BooksPage";
+import Header from "./components/header/Header";
 
 function App() {
-
-
-
   return (
     <BooksProvider>
-      <div className="App">
-        <h1>Ma bibliothèque</h1>
-        <BooksPage />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/livres" element={<BooksPage />} />
+          </Routes>
+        </div>
+      </Router>
     </BooksProvider>
   );
 }
