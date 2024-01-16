8b532500be0e26a5f8ae933aaee60ed3c62a8b6e
florian <furnari.florian@hotmail.fr>
2024-01-16 17:21:23 +0100

    mise en place de ascendant descendant

--- src/components/books/BooksPage.jsx
+++ src/components/books/BooksPage.jsx
@@ -1,19 +1,24 @@
-
+import './BooksPage.css';
 import { useContext } from 'react';
 import { BooksContext } from '../../utils/context/BooksContext';
 
 const BooksPage = () => {
-  const {tableBooks} = useContext(BooksContext);
+  const { tableBooks, sortOrder, toggleSortOrder } = useContext(BooksContext);
 
   return (
     <div>
       <h2>Liste des livres :</h2>
       <div>
+        <div>
+          <span onClick={toggleSortOrder}>
+            Auteur {sortOrder === 'asc' ? '▲' : '▼'}
+          </span>
+        </div>
         {tableBooks.map((book) => (
           <div key={book.id}>
-            <div>link : {book.link}</div> 
-            <div>titre : {book.title}</div> 
-            <div>autheur : {book.author}</div>
+            <img src={book.imageLink} alt={book.title} /> 
+            <div>Titre : {book.title}</div> 
+            <div>Auteur : {book.author}</div>
           </div>
         ))}
       </div>
--- src/utils/context/BooksContext.jsx
+++ src/utils/context/BooksContext.jsx
@@ -1,16 +1,39 @@
-
-import { createContext, useState, } from 'react';
- import { books } from '../../models/Books';
+import { createContext, useState } from 'react';
+import { books } from '../../models/Books';
 
 export const BooksContext = createContext();
 
-
 export const BooksProvider = ({ children }) => {
-  const [tableBooks, setBook] = useState(books)
-  console.log(tableBooks)
+  const [tableBooks, setTableBooks] = useState(books);
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const sortBooksByAuthor = (books, order) => {
+    return books.sort((a, b) => {
+      const authorA = a.author.toLowerCase();
+      const authorB = b.author.toLowerCase();
+
+      if (order === 'asc') {
+        return authorA.localeCompare(authorB);
+      } else {
+        return authorB.localeCompare(authorA);
+      }
+    });
+  };
+
+  const sortedBooks = sortBooksByAuthor([...tableBooks], sortOrder);
+
+  const contextValue = {
+    tableBooks: sortedBooks,
+    sortOrder,
+    toggleSortOrder,
+  };
 
   return (
-    <BooksContext.Provider value={{tableBooks}}>
+    <BooksContext.Provider value={contextValue}>
       {children}
     </BooksContext.Provider>
   );
