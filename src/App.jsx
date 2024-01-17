828d9a1f9ea0476906d631719d58d2e979a6f233
florian <furnari.florian@hotmail.fr>
2024-01-17 10:00:26 +0100

    lecon sur usecontext et createcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BooksProvider } from "./utils/context/BooksContext";
-import BooksPage from "./components/books/BooksPage";
-import Header from "./components/header/Header";
+import Profile from "./components/Profile/Profile";
+import SmileyBox from "./components/Smiley/SmileyBox";
+import { SmileysProvider } from "./utils/context";
 
 function App() {
   return (
-    <BooksProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/livres" element={<BooksPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </BooksProvider>
+    <div className="app">
+      <SmileysProvider>
+        <Profile />
+        <SmileyBox />
+      </SmileysProvider>
+    </div>
   );
 }
 
