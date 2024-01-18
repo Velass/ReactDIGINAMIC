7c82f2467082049156357d3ef30f80786d318009
florian <furnari.florian@hotmail.fr>
2024-01-18 13:37:28 +0100

    test 1 du useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import "./App.css";
-import Smiley from "./components/Smiley";
-import { smileys } from "./models";
+import { useReducer } from 'react';
 
-function App() {
+// Définition du reducer
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const Counter = () => {
+  const [count, dispatch] = useReducer(counterReducer, 0);
+
   return (
-    <div className="app">
-      <h2>Quelle est ton humeur du jour ?</h2>
-      <div className="smileys-box">
-        {smileys.map((smiley) => (
-          <Smiley key={smiley.id} smiley={smiley}></Smiley>
-        ))}
-      </div>
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Counter;
